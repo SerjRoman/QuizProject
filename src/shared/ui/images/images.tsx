1efa0c7f968b12b5d9d474ef549ccf368b7bdf28
SerjRoman <sergeyroman26@gmail.com>
2025-07-11 12:24:07 +0100

    Added logo image

--- src/shared/ui/images/images.tsx
+++ src/shared/ui/images/images.tsx
@@ -1,7 +1,9 @@
 import privateBackground from "@assets/adminBg.png";
 import loginBackground from "@assets/loginBg.png";
+import logo from "@assets/logo.png";
 
 export const Images = {
 	privateBackground,
 	loginBackground,
+	logo,
 };
