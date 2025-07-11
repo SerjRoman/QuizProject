5b8851ec3b9cdbe5471df0ff048e49056273a8e3
SerjRoman <sergeyroman26@gmail.com>
2025-07-11 17:21:31 +0100

    feat: implement form styling

--- src/shared/ui/form/form.tsx
+++ src/shared/ui/form/form.tsx
@@ -1,11 +1,17 @@
+import { clsx } from "clsx";
 import { FormProvider, type FieldValues } from "react-hook-form";
+import styles from "./form.module.css";
 import type { IFormProps } from "./form.types";
 
 export function Form<T extends FieldValues>(props: IFormProps<T>) {
 	const { methods, onSubmit, children, formProps } = props;
 	return (
 		<FormProvider {...methods}>
-			<form onSubmit={methods.handleSubmit(onSubmit)} {...formProps}>
+			<form
+				onSubmit={methods.handleSubmit(onSubmit)}
+				{...formProps}
+				className={clsx(styles.form, formProps?.className)}
+			>
 				{children}
 			</form>
 		</FormProvider>
