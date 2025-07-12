10bae20ffd13485e977dc8cd2b98674a492ec3e1
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-07-12 19:02:30 +0300

    fix name

--- src/entities/user/model/constants/teacher-header-map.ts
+++ src/entities/user/model/constants/teacher-header-map.ts
@@ -1,27 +1,27 @@
-import { ADMIN_HEADER_NAVIGATION_MAP } from "@/shared/model/constants";
+import { TEACHER_HEADER_NAVIGATION_MAP } from "@/shared/model/constants";
 import { Icons } from "@/shared/ui";
 
 export const TEACHER_HEADER_MAP = [
 	{
-		path: ADMIN_HEADER_NAVIGATION_MAP.libraries,
+		path: TEACHER_HEADER_NAVIGATION_MAP.libraries,
 		title: "Libraries",
 		icon: Icons.Library,
 		dataSoruce: ["All", "Created", "Copied", "Folders", "Favourite"],
 	},
 	{
-		path: ADMIN_HEADER_NAVIGATION_MAP.reports,
+		path: TEACHER_HEADER_NAVIGATION_MAP.reports,
 		title: "Reports",
 		icon: Icons.Library,
 		dataSoruce: ["All", "Class", "Room", "Quiz", "Favourite", "Custom"],
 	},
 	{
-		path: ADMIN_HEADER_NAVIGATION_MAP.classes,
+		path: TEACHER_HEADER_NAVIGATION_MAP.classes,
 		title: "Classes",
 		icon: Icons.Library,
 		dataSoruce: ["All", "Favourite"],
 	},
 	{
-		path: ADMIN_HEADER_NAVIGATION_MAP.community,
+		path: TEACHER_HEADER_NAVIGATION_MAP.community,
 		title: "Community",
 		icon: Icons.Library,
 	},
