c0737e58e7b3c365402c5df4a9b38cd63fbe407f
SerjRoman <sergeyroman26@gmail.com>
2025-07-14 16:00:12 +0100

    implemented quiz search for library page

---
+++ src/features/teacher/library/quiz-search/ui/index.ts
@@ -0,0 +1,1 @@
+export { QuizSearch } from "./quiz-search";
---
+++ src/features/teacher/library/quiz-search/ui/quiz-search.tsx
@@ -0,0 +1,18 @@
+import { search } from "@/entities/quiz";
+import { useAppDispatch } from "@/shared/lib";
+import { Icons, Input } from "@/shared/ui";
+import styles from "./quiz-search.module.css";
+
+export function QuizSearch() {
+	const dispatch = useAppDispatch();
+	return (
+		<Input
+			placeholder="Start typing..."
+			iconRight={<Icons.Search className={styles.icon} />}
+			className={styles.search}
+			onChange={(e) => {
+				dispatch(search({ search: e.target.value }));
+			}}
+		/>
+	);
+}
