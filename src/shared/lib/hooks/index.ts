6d3c14741ca65dc8562f8675a811a5b18c83872d
SerjRoman <sergeyroman26@gmail.com>
2025-07-17 20:56:07 +0100

    Use debounce hook implemented to work with search and other debounce value

--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -2,3 +2,4 @@ export { useModal } from "./use-modal";
 export { useClickOutside } from "./use-click-outside";
 export { useAppSelector } from "./use-app-selector";
 export { useAppDispatch } from "./use-app-dispatch";
+export { useDebounce } from "./use-debounce";
